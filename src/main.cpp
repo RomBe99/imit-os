2404ffaaa79829d2f3a799610642711a8502dec6
Roman <romanbelinsky99@gmail.com>
2020-04-09 17:32:30 +0600

    Now all params getting from console

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,29 @@
 #include "../headers/file-io-tasks/file_with_hole.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    create_file_with_hole();
+    // Длина первой строки
+    int buf1Length = atoi(argv[1]);
 
-    return 0;
+    if (buf1Length < 0) {
+        return EXIT_FAILURE;
+    }
+
+    // Длина второй строки
+    int buf2Length = atoi(argv[2]);
+
+    if (buf2Length < 0) {
+        return EXIT_FAILURE;
+    }
+
+    // Длина "дырки"
+    int seekSize = atoi(argv[3]);
+
+    if (seekSize < 0) {
+        return EXIT_FAILURE;
+    }
+
+    create_file_with_hole(buf1Length, buf2Length, seekSize, argv[4], argv[5]);
+
+    return EXIT_SUCCESS;
 }
